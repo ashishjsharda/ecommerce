510f006f92a450626ff947adf49db73e63a25f2b
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-13 11:39:12 -0500

    update models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+    def __str__(self):
+        return self.name
+
