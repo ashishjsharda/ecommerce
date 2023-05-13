187ad4b13632fff8bed09c1eac7b81e3a1bf7b67
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-13 11:52:02 -0500

    apply migrations

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,5 +4,6 @@ from catalog.views import product_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', product_list, name='product_list'),  # Add this line
     path('products/', product_list, name='product_list'),
 ]
