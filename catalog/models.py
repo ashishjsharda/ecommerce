822f592d26c0054f089007d7a2991836069bb3e9
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-13 11:45:15 -0500

    add templated ,update url and add html

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    image = models.ImageField(upload_to='products')
 
     def __str__(self):
         return self.name
-
